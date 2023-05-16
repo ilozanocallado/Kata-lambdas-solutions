4c8c409401de218291a7e090555b14eaf4b12348
Pablo Conca <pabloconcagran@gmail.com>
2023-05-16 13:12:42 +0200

    part 4

--- src/main/java/kata/lambdas/part4/DragonsCurve.java
+++ src/main/java/kata/lambdas/part4/DragonsCurve.java
@@ -2,23 +2,40 @@ package kata.lambdas.part4;
 
 import java.util.function.IntFunction;
 import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
 
 public class DragonsCurve {
-    public IntFunction<String> mapFunction = ; //Make the function; map the chars to Strings
-    //a -> aRbFR, b -> LFaLb, otherwise -> itself
-    /**
-     * Make the curve; stream the chars repeatedly (starting with Fa) through the mapFunction n times
-     * Then remove the a and b (createFilter function is useful for that)
-     */
+    public IntFunction<String> mapFunction = c -> {
+        if (c == 'a') return "aRbFR";
+        if (c == 'b') return "LFaLb";
+        return String.valueOf((char) c);
+    };
+
     public String createCurve(int n) {
-        "Fa".chars(); //That's an IntStream with 'F' and 'a' ready to be acted upon
-        return "Fa";
+        String result = "Fa";
+        for (int i = 0; i < n; i++) {
+            result = result.chars().mapToObj(mapFunction).collect(Collectors.joining());
+        }
+        result = result.chars()
+                .filter(createFilter('a', false))
+                .filter(createFilter('b', false))
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+        return result;
     }
 
-    /**
-     * How many of the specified char are in the given curve?
-     * Hint: createFilter could be useful for this
-     */
     public long howMany(char c, String curve) {
-        return 4L; //Determined by die roll; guaranteed to be random
+        return curve.chars()
+                .filter(createFilter(c, true))
+                .count();
     }
+
+    public IntPredicate createFilter(char filterWhat, boolean keep) {
+        return c -> (keep) ? c == filterWhat : c != filterWhat;
+    }
+
+    public static void main(String[] args) {
+        DragonsCurve dc = new DragonsCurve();
+        System.out.println(dc.createCurve(3));
+    }
+}
